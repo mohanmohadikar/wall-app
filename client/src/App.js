339053e41852af7e9cfe4beb3743dcd3ca7b05cb
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2021-01-28 22:33:26 +0530

    setup Post module.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import activities from './images/activities.png';
 const App = () =>{
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container>
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Wall.app</Typography>
+                <img src={activities} alt="Activities" height="60"/>
+            </AppBar>
+        </Container>
     );
 }
 
