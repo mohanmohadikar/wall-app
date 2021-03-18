6ba9c40bbee78e5104b9486c12f84bf4d40ce4ad
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2021-03-18 16:42:52 +0530

    add delete post feature.

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,33 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles.js';
-import { createPost } from '../../actions/posts.js';
+import { createPost, updatePost } from '../../actions/posts.js';
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
 
     const [postData, setPostData] = useState({
         creator:'', title:'', message:'', tags:'', selectedFile:''
     });
 
+    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id===currentId) : null);
+
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        if(post) setPostData(post)
+    }, [post])
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        dispatch(createPost(postData));
+
+        if(currentId) dispatch(updatePost(currentId, postData));
+        else dispatch(createPost(postData));
+        clear();
     }
 
     const clear = () => {
 
+        setCurrentId(null);
+
+        setPostData({
+            creator:'', title:'', message:'', tags:'', selectedFile:''
+        });
+    
     }
 
     return(
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={ `${classes.root} ${classes.form}` } onSubmit={handleSubmit}>
-                <Typography variant="h6">Share your post</Typography>
+                <Typography variant="h6">{ currentId ? 'Edit' : 'Share' } your post</Typography>
                 <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator}
                     onChange={(e) => setPostData({ ...postData, creator:e.target.value})}
                 />
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
 import Post from './Post/Post.js';
 import useStyles from './styles.js';
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
     const classes = useStyles();
     const posts = useSelector((state) => state.posts);
 
     console.log(posts);
 
     return(
-        <>
-            <h1>POSTS</h1>
-            <Post />
-            <Post />
-            <Post />
-        </>
-        
+        !posts.length? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {
+                    posts.map((post) => (
+                        <Grid key={post._id} item xs={12} sm={6} >
+                            <Post post={post} setCurrentId={setCurrentId} />
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        )
     );
 }
 
